9fe8bb07f65c3be9819a0efe9d8bd8e32885f372
devpfan <pfabian@outlook.com>
2026-02-04 19:16:47 -0500

    estructura inicial con springboot initializer

---
+++ src/main/java/com/redcomunitaria/backend/RedComunitariaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redcomunitaria.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedComunitariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedComunitariaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redcomunitaria/backend/RedComunitariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcomunitaria.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedComunitariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
